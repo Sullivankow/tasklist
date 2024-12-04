d12c8fd86d7cdec92680fe354c3a063f15e43b8a
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2024-12-04 11:52:36 +0100

    Insatallation package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,8 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <ChakraProvider>
         {children}
+        </ChakraProvider>
       </body>
     </html>
   );
