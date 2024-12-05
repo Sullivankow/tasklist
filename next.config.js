1c609101a24fbd39d0d86807ce7cfc0f1c70cc63
SundlyDev <sundly@live.fr>
2024-12-05 13:09:58 +0100

    MODIF configuration + page d'accueil ok + MAJ version react

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Ajoutez vos options de configuration ici
+  reactStrictMode: true,
+  // Par exemple : 
+  // pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+};
+
+module.exports = {
+  reactStrictMode: true,
+};
+
